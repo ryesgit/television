169ef58b1d5180c2400ccc1e28fd639f5930823f
ryesgit <128949576+ryesgit@users.noreply.github.com>
2023-05-14 11:25:16 +0800

    Initial commit

---
+++ backend/Television.py
@@ -0,0 +1,10 @@
+'''
+filename: Television.py
+
+Contains the Television class.
+Accepts three arguments:
+- Channel > The television instance's initial channel (Available slots from 1-10),
+- Volume Level > The instance's initial volume level (Available slots, multiples of 10 up to 100)
+- On > The instance's initial on state. Is a boolean
+
+'''
