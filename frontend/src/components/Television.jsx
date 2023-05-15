e3240b7230a052b367d35145f66e92aed5253f59
ryesgit <128949576+ryesgit@users.noreply.github.com>
2023-05-16 00:24:38 +0800

    Use Youtube component package for video

--- frontend/src/components/Television.jsx
+++ frontend/src/components/Television.jsx
@@ -1,15 +1,30 @@
-import { useState } from 'react'
-import YouTubeEmbed from './YouTubeEmbed.jsx'
+import { useEffect, useState } from 'react'
+
+import YouTube from "react-youtube";
 
 const Television = () => {
   const [volume, setVolume] = useState(100);
-  const [channel, setChannel] = useState(0);
+  const [channel, setChannel] = useState('LaBY6Jdhu5Q');
   const [on, setOn] = useState(false);
-  
+  const [opts, setOpts] = useState({});
+  useEffect(() => {
+    const opts = {
+      height: '390',
+      width: '100%',
+      playerVars: {
+        // https://developers.google.com/youtube/player_parameters
+        autoplay: 1,
+        mute: 1
+      },
+    }
+
+    setOpts(opts);
+  }, []);  
+
   return (
     <>
         <aside className=' p-4 bg-black'>
-            <YouTubeEmbed src={"https://www.youtube.com/embed/LaBY6Jdhu5Q"} title={"From zero to hero with Microsoft Graph API – July 2021"}/>
+            <YouTube videoId={channel} opts={opts}/>
 
             <div className=' flex justify-around items-center pt-2'>
                 <button>Volume Up</button>
